62e8eb53208e59ce884092e95fed0614fcbd6106
Renato Aloi <renato.aloi@gmail.com>
2022-12-13 19:20:34 -0300

    Ajuste na configuração do modo de operação

--- UserConfig.ino
+++ UserConfig.ino
@@ -1,21 +1,25 @@
 
-struct UserConfig {
-  int  id;
+struct UserConfig
+{
+  int id;
   char ssid[20];
   char password[20];
   char dispositivo[20];
   bool modoAP;
+  byte modoOperacao;
 };
 
-int UserId = 91284;
+int UserId = 91285;
 int eeAddress = 0;
 static struct UserConfig userConfig;
 
-void initUserConfig() {
+void initUserConfig()
+{
   EEPROM.begin(512);
 }
 
-void SaveUserConfig(char* _ssid, char* _password, char* _dispositivo) {
+void SaveUserConfig(char *_ssid, char *_password, char *_dispositivo)
+{
   userConfig.id = UserId;
   strcpy(userConfig.ssid, _ssid);
   strcpy(userConfig.password, _password);
@@ -25,19 +29,22 @@ void SaveUserConfig(char* _ssid, char* _password, char* _dispositivo) {
   EEPROM.commit();
 }
 
-void SaveModoAP() {
+void SaveModoAP()
+{
   userConfig.modoAP = true;
   EEPROM.put(eeAddress, userConfig);
   EEPROM.commit();
 }
 
-void TurnOffModoAP() {
+void TurnOffModoAP()
+{
   userConfig.modoAP = false;
   EEPROM.put(eeAddress, userConfig);
   EEPROM.commit();
 }
 
-void SaveWifiConfig(const char* _ssid, const char* _pass) {
+void SaveWifiConfig(const char *_ssid, const char *_pass)
+{
   GetUserConfig();
   strcpy(userConfig.ssid, _ssid);
   strcpy(userConfig.password, _pass);
@@ -45,28 +52,34 @@ void SaveWifiConfig(const char* _ssid, const char* _pass) {
   EEPROM.commit();
 }
 
-void SaveDevice(const char* _device) {
+void SaveDevice(const char *_device)
+{
   GetUserConfig();
   strcpy(userConfig.dispositivo, _device);
   EEPROM.put(eeAddress, userConfig);
   EEPROM.commit();
 }
 
-char* GetWifiSsid() {
+char *GetWifiSsid()
+{
   return userConfig.ssid;
 }
 
-char* GetWifiPassword() {
+char *GetWifiPassword()
+{
   return userConfig.password;
 }
 
-char* GetDispositivo() {
+char *GetDispositivo()
+{
   return userConfig.dispositivo;
 }
 
-void GetUserConfig() {
+void GetUserConfig()
+{
   EEPROM.get(eeAddress, userConfig);
-  if (DEBUG) {
+  if (DEBUG)
+  {
     Serial.print("id: ");
     Serial.println(userConfig.id);
     Serial.print("ssid: ");
@@ -77,33 +90,41 @@ void GetUserConfig() {
     Serial.println(userConfig.dispositivo);
     Serial.print("Modo AP ");
     Serial.println(userConfig.modoAP ? "ligado!" : "desligado!");
+    Serial.print("Modo de Operacao ");
+    Serial.println(userConfig.modoOperacao == 1 ? "Alexa" : userConfig.modoOperacao == 2 ? "Smartphone"
+                                                                                         : "Nao implementado");
   }
 }
 
-bool isUserConfigModeAP() {
+bool isUserConfigModeAP()
+{
   return userConfig.modoAP;
 }
 
-void ClearUserConfig() {
-  for (int i = 0; i < 512; i++) {
+void ClearUserConfig()
+{
+  for (int i = 0; i < 512; i++)
+  {
     EEPROM.write(i, 0);
   }
   EEPROM.commit();
 }
 
-void configUser() {
+void configUser()
+{
   initUserConfig();
   GetUserConfig();
-  if (userConfig.id != UserId) {
+  if (userConfig.id != UserId)
+  {
     ClearUserConfig();
-    SaveUserConfig("", "", "");
+    SaveUserConfig("", "", "", 0);
     ResetDevice();
   }
 }
 
-bool CheckUserConfig() {
-  return 
-       strlen(userConfig.ssid) != 0 
-    && strlen(userConfig.password) != 0
-    && strlen(userConfig.dispositivo) != 0;
+bool CheckUserConfig()
+{
+  return strlen(userConfig.ssid) != 0 && strlen(userConfig.password) != 0 &&
+         ((strlen(userConfig.dispositivo) != 0 && userConfig.modoOperacao == 1) ||
+          (userConfig.modoOperacao != 2));
 }
